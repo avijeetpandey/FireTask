b6a7bd272323df7888678025e486d1ebbd5af7bb
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-04-25 17:21:08 +0530

    Initial Commit

---
+++ FireTask/FireTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireTaskApp.swift
+//  FireTask
+//
+//  Created by Avijeet Pandey on 25/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FireTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
